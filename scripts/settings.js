ab7c7c6e81cdc98bbdf7487a5312be8fecfb56b7
Chris <chrisk123999@gmail.com>
2023-02-18 15:05:17 -0800

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -3,7 +3,7 @@ export function registerSettings() {
 	game.settings.register(moduleName, 'Armor of Agathys', {
 		'name': 'Armor of Agathys Automation',
 		'hint': 'Armor of Agathys Automation',
-		'default' false,
+		'default': false,
 		'scope': 'world',
 		'config': true,
 		'type': Boolean
