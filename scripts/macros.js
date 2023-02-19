6946638d27491ce3a27abdde20e1d130c9987c43
Chris <chrisk123999@gmail.com>
2023-02-19 14:21:49 -0800

    Charm Person

--- scripts/macros.js
+++ scripts/macros.js
@@ -2,6 +2,7 @@ import {armorOfAgathys} from './macros/spells/armorOfAgathys/armorOfAgathys.js';
 import {callLightning} from './macros/spells/callLightning/callLightning.js';
 import {conditionResistanceEarly, conditionResistanceLate} from './macros/mechanics/conditionResistance/conditionResistance.js';
 import {conditionVulnerabilityEarly, conditionVulnerabilityLate} from './macros/mechanics/conditionVulnerability/conditionVulnerability.js';
+import {charmPerson} from './macros/spells/charmPerson/charmPerson.js';
 export let macros = {
 	'armorOfAgathys': armorOfAgathys,
 	'callLightning': callLightning,
@@ -9,7 +10,5 @@ export let macros = {
 	'conditionResistanceLate': conditionResistanceLate,
 	'conditionVulnerabilityEarly': conditionVulnerabilityEarly,
 	'conditionVulnerabilityLate': conditionVulnerabilityLate,
-
-
-	
+	'charmPerson': charmPerson
 }
---
+++ scripts/macros/spells/charmPerson/charmPerson.js
@@ -0,0 +1,31 @@
+import {chris} from '../../../helperFunctions.js';
+export async function charmPerson(workflow) {
+	if (workflow.targets.size === 0) return;
+	if (game.combat === null || game.combat === undefined) return;
+	let effectData = {
+		'label': 'Condition Advantage',
+		'icon': 'icons/magic/time/arrows-circling-green.webp',
+		'duration': {
+			'turns': 1
+		},
+		'changes': [
+			{
+				'key': 'flags.midi-qol.advantage.ability.save.all',
+				'value': '1',
+				'mode': 5,
+				'priority': 120
+			}
+		],
+		'flags': {
+			'dae': {
+				'specialDuration': [
+					'isSave'
+				]
+			}
+		}
+	};
+	let targets = Array.from(workflow.targets);
+	for (let i of targets) {
+		await chris.createEffect(i.actor, effectData);
+	}
+}
