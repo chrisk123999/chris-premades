d7f28596e742eed53c18a5b01f60d4f4fad775ed
Chris <chrisk123999@gmail.com>
2023-04-07 23:17:19 -0700

    Update auraOfVitality.js

--- scripts/macros/spells/auraOfVitality.js
+++ scripts/macros/spells/auraOfVitality.js
@@ -6,7 +6,6 @@ export async function auraOfVitality({speaker, actor, token, character, item, ar
     featureData.system.description.value = chris.getItemDescription('CPR - Descriptions', 'Aura of Vitality Healing');
     async function effectMacro () {
 		await warpgate.revert(token.document, 'Aura of Vitality');
-        await chrisPremades.macros.heatMetal.removed(effect);
 	}
     let effectData = {
         'label': 'Aura of Vitality',
