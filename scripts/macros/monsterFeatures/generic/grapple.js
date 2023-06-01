c804fbc0ca7ba146d474dd4f7e03687c01c97eda
Autumn225 <134600398+Autumn225@users.noreply.github.com>
2023-06-01 12:24:03 -0700

    Grapple update (#68)
    * Bigby's Hand
    
    * Homunculus Summon, bug fixed
    
    Added Homunculus Servant infusion, Bigby's hand bug fixes.
    
    * Homunculus Servant
    
    Added Homunculus Servant infusion, Bigby's hand bug fixes.
    
    * Minor bug fix in progress
    
    * Revert "Minor bug fix in progress"
    
    This reverts commit 6c4631b9d1485b316a46dfb33ece41f8275e46aa.
    
    * Grapple Update
    
    Updated grapple to check for target size

--- scripts/macros/monsterFeatures/generic/grapple.js
+++ scripts/macros/monsterFeatures/generic/grapple.js
@@ -1,8 +1,12 @@
 import {chris} from '../../../helperFunctions.js';
 export async function grapple({speaker, actor, token, character, item, args, scope, workflow}) {
     if (workflow.targets.size != 1) return;
-    let sourceRoll = await workflow.actor.rollSkill('ath');
     let targetActor = workflow.targets.first().actor;
+    if ((chris.getSize(targetActor)) > (chris.getSize(actor) + 1)) {
+        ui.notifications.info('Target is too big!');
+        return;
+    }
+    let sourceRoll = await workflow.actor.rollSkill('ath');
     let targetRoll;
     if (targetActor.system.skills.acr.total >= targetActor.system.skills.ath.total) {
         targetRoll = await targetActor.rollSkill('acr');
