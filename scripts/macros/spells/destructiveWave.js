611157d8a770b8d206d231dcba340fb6fe42c088
Autumn225 <134600398+Autumn225@users.noreply.github.com>
2023-06-19 23:44:06 -0700

    Destructive Wave (#86)
    * Relentless Endurance
    
    Relentless Endurance, strength of the grave bug fix
    
    * Destructive Wave
    
    Destructive wave.
    
    * Bug fixes
    
    Prone being macro.ce

---
+++ scripts/macros/spells/destructiveWave.js
@@ -0,0 +1,16 @@
+import {chris} from '../../helperFunctions.js';
+import {queue} from '../../queue.js';
+export async function destructiveWave({speaker, actor, token, character, item, args, scope, workflow}) {
+    console.log(workflow);
+    console.log(workflow.failedSaves);
+    if (!(workflow.failedSaves.size > 0)) return;
+    let queueSetup = await queue.setup(workflow.item.uuid, 'destructiveWave', 50);
+    if (!queueSetup) return;
+    let selection = await chris.dialog('What damage type?', [['Radiant', 'radiant'], ['Necrotic', 'necrotic']]);
+    if (!selection) selection = 'radiant';
+    let damageFormula = workflow.damageRoll._formula.replace('none', selection);
+    let damageRoll = await new Roll(damageFormula).roll({async: true});
+    await workflow.setDamageRoll(damageRoll);
+    new Sequence().effect().atLocation(workflow.token).file('jb2a.thunderwave.center.blue').scale(2.2).playbackRate(0.5).play();
+    queue.remove(workflow.item.uuid);
+}
