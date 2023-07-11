54580ee011c76a0996aede982b944451e48adbb2
Chris <chrisk123999@gmail.com>
2023-07-11 01:24:46 -0700

    Update enhancedBond.js

--- scripts/macros/classFeatures/druid/circleOfWildfire/enhancedBond.js
+++ scripts/macros/classFeatures/druid/circleOfWildfire/enhancedBond.js
@@ -1,6 +1,5 @@
 import {chris} from '../../../../helperFunctions.js';
 export function enhancedBond(actor) {
     let effect = chris.findEffect(actor, 'Summon Wildfire Spirit');
-    if (!effect) return false;
-    return true;
+    return !!effect;
 }
