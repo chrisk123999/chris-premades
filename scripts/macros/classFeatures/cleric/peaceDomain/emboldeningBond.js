fa21c08078ccc6aee7cc74bf1c5d3cbddada56b2
Chris <chrisk123999@gmail.com>
2023-07-14 19:06:48 -0700

    Update emboldeningBond.js

--- scripts/macros/classFeatures/cleric/peaceDomain/emboldeningBond.js
+++ scripts/macros/classFeatures/cleric/peaceDomain/emboldeningBond.js
@@ -1,5 +1,4 @@
 import {chris} from '../../../../helperFunctions.js';
-import {queue} from '../../../../queue.js';
 async function effectMacro() {
     await chrisPremades.macros.emboldeningBond.remove(token);
 }
@@ -52,19 +51,11 @@ let effectData = {
     }
 };
 async function turn(token, origin, effect) {
-    let queueSetup = await queue.setup('emboldeningBond', effect.uuid, 50);
-    if (!queueSetup) return;
     let effect2 = chris.findEffect(token.actor, 'Emboldening Bond Bonus');
-    if (effect2) {
-        queue.remove('emboldeningBond');
-        return;
-    }
+    if (effect2) return;
     let maxDistance = effect.flags['chris-premades']?.feature?.emboldeningBond?.expansiveBond ?? 30;
-    let nearbyTargets = await chris.findNearby(token, maxDistance, 'all', true).concat(token).filter(i => i.actor.effects.find(j => j.origin === origin.uuid && j.label === 'Emboldening Bond'));
-    if (nearbyTargets.length < 2) {
-        queue.remove('emboldeningBond');
-        return;
-    }
+    let nearbyTargets = await chris.findNearby(token, maxDistance, 'all', true).concat(token).filter(t => t.actor.effects.find(e => e.origin === origin.uuid && e.label === 'Emboldening Bond'));
+    if (nearbyTargets.length < 2) return;
     let effectData2 = duplicate(effectData);
     setProperty(effectData2, 'flags.chris-premades.feature.emboldeningBond.sourceTokenUuid', token.document.uuid);
     setProperty(effectData2, 'origin', origin.uuid);
@@ -72,60 +63,43 @@ async function turn(token, origin, effect) {
     setProperty(effectData2, 'duration.seconds', effect.duration.seconds);
     if (token.actor.flags['chris-premades']?.feature?.expansiveBond) setProperty(effectData2, 'flags.chris-premades.feature.emboldeningBond.expansiveBond', 60);
     await chris.createEffect(token.actor, effectData2);
-    queue.remove('emboldeningBond');
 }
-async function checkBonus(token) {
-    let effects = token.actor.effects.filter(i => i.label === 'Emboldening Bond');
-    if (effects.length === 0) return;
-    let effect2 = chris.findEffect(token.actor, 'Emboldening Bond Bonus');
-    let targetMap = {};
-    if (effect2) {
-        for (let effect of effects) {
-            let maxDistance = effect.flags['chris-premades']?.feature?.emboldeningBond?.expansiveBond ?? 30;
-            let origin;
-            if (effect.origin) origin = await fromUuid(effect.origin);
-            if (!origin) continue;
-            let nearbyTargets = targetMap[effect.uuid];
-            if (!nearbyTargets) {
-                targetMap[effect.uuid] = await chris.findNearby(token.object, maxDistance, 'all', true).concat(token.object).filter(i => i.actor.effects.find(j => j.origin === origin.uuid && j.label === 'Emboldening Bond'));
-                nearbyTargets = targetMap[effect.uuid];
+async function checkBonus(effect) {
+    if (!effect.origin) return;
+    let tokens = canvas.scene.tokens.filter(t => t.actor.effects.find(e => e.label == 'Emboldening Bond' && e.origin === effect.origin));
+    for (let token of tokens) {
+        let effect3 = chris.findEffect(token.actor, 'Emboldening Bond');
+        let distance = effect3.flags['chris-premades']?.feature?.emboldeningBond?.expansiveBond ?? 30;
+        let nearbyTargets = chris.findNearby(token.object, distance, 'all', true).concat(token.object).filter(t => t.actor.effects.find(e => e.label === 'Emboldening Bond'));
+        let effect2 = chris.findEffect(token.actor, 'Emboldening Bond Bonus');
+        if (nearbyTargets.length < 2) {
+            if (effect2) {
+                await chris.updateEffect(effect2, {'disabled': true});
+                await chris.removeEffect(effect2);
             }
-            if (nearbyTargets.length < 2) continue;
-            return;
+        } else {
+            if (effect2) continue;
+            if (chris.inCombat()) {
+                let currentTurn = game.combat.round + '-' + game.combat.turn;
+                let previousTurn = effect3.flags['chris-premades']?.feature?.emboldeningBond.turn;
+                if (currentTurn === previousTurn) continue;
+            }
+            let effectData2 = duplicate(effectData);
+            setProperty(effectData2, 'flags.chris-premades.feature.emboldeningBond.sourceTokenUuid', effect3.flags['chris-premades']?.feature?.emboldeningBond?.sourceTokenUuid);
+            setProperty(effectData2, 'origin', effect3.origin);
+            setProperty(effectData2, 'icon', effect3.icon);
+            setProperty(effectData2, 'duration.seconds', effect3.duration.seconds);
+            if (effect3.flags['chris-premades']?.feature?.emboldeningBond?.expansiveBond) setProperty(effectData2, 'flags.chris-premades.feature.emboldeningBond.expansiveBond', 60);
+            await chris.createEffect(token.actor, effectData2);
         }
-        await chris.updateEffect(effect2, {'disabled': true});
-        await chris.removeEffect(effect2);
-    }
-    for (let effect of effects) {
-        if (chris.inCombat()) {
-            let currentTurn = game.combat.round + '-' + game.combat.turn;
-            let previousTurn = effect.flags['chris-premades']?.feature?.emboldeningBond.turn;
-            if (currentTurn === previousTurn) return;
-        }
-        let maxDistance = effect.flags['chris-premades']?.feature?.emboldeningBond?.expansiveBond ?? 30;
-        let origin;
-        if (effect.origin) origin = await fromUuid(effect.origin);
-        if (!origin) continue;
-        let nearbyTargets = targetMap[effect.uuid];
-        if (!nearbyTargets) {
-            targetMap[effect.uuid] = await chris.findNearby(token.object, maxDistance, 'all', true).concat(token.object).filter(i => i.actor.effects.find(j => j.origin === origin.uuid && j.label === 'Emboldening Bond'));
-            nearbyTargets = targetMap[effect.uuid];
-        }
-        if (nearbyTargets.length < 2) continue;
-        let effectData2 = duplicate(effectData);
-        setProperty(effectData2, 'flags.chris-premades.feature.emboldeningBond.sourceTokenUuid', token.uuid);
-        setProperty(effectData2, 'origin', origin.uuid);
-        setProperty(effectData2, 'icon', origin.img);
-        setProperty(effectData2, 'duration.seconds', effect.duration.seconds);
-        if (effect.flags['chris-premades']?.feature?.emboldeningBond?.expansiveBond) setProperty(effectData2, 'flags.chris-premades.feature.emboldeningBond.expansiveBond', 60);
-        await chris.createEffect(token.actor, effectData2);
-        return;
     }
 }
 async function move(token, changes) {
     if (game.settings.get('chris-premades', 'LastGM') != game.user.id) return;
     if (!changes.x && !changes.y && !changes.elevation) return;
-    checkBonus(token);
+    let effect = chris.findEffect(token.actor, 'Emboldening Bond');
+    if (!effect) return;
+    checkBonus(effect);
 }
 async function item({speaker, actor, token, character, item, args, scope, workflow}) {
     if (workflow.targets.size === 0) return;
@@ -164,25 +138,14 @@ async function item({speaker, actor, token, character, item, args, scope, workfl
         setProperty(effectData3, 'flags.chris-premades.feature.emboldeningBond.expansiveBond', 60);
     }
     for (let token of Array.from(workflow.targets)) {
-        if (chris.inCombat()) {
-            let effect = chris.findEffect(token.actor, 'Emboldening Bond');
-            if (effect) {
-                let previousTurn = effect.flags['chris-premades']?.feature?.emboldeningBond.turn;
-                if (previousTurn) setProperty(effectData3, 'flags.chris-premades.feature.emboldeningBond.turn', previousTurn);
-            }
-        }
         await chris.createEffect(token.actor, effectData3);
-        if (chris.findEffect(token.actor, 'Emboldening Bond Bonus')) continue;
         await chris.createEffect(token.actor, effectData2);
     }
 }
 async function remove(token) {
-    if (!chris.inCombat()) return;
-    let effects = token.actor.effects.filter(i => i.label === 'Emboldening Bond');
-    if (effects.length === 0) return;
-    for (let effect of effects) {
-        await effect.setFlag('chris-premades', 'feature.emboldeningBond.turn', game.combat.round + '-' + game.combat.turn);
-    }
+    let effect = chris.findEffect(token.actor, 'Emboldening Bond');
+    if (!effect) return;
+    await effect.setFlag('chris-premades', 'feature.emboldeningBond.turn', game.combat.round + '-' + game.combat.turn);
 }
 export let emboldeningBond = {
     'turn': turn,
