7eb9a39dda3f2f7115a26e361f8757288fc5632a
Chris <chrisk123999@gmail.com>
2023-10-08 02:40:25 -0700

    Sync

--- scripts/macros/spells/beaconOfHope.js
+++ scripts/macros/spells/beaconOfHope.js
@@ -5,15 +5,15 @@ export async function beaconOfHope(token, {item, workflow, ditem}) {
     let effect = chris.findEffect(token.actor, 'Beacon of Hope');
     if (!effect) return;
     if (!workflow.damageRoll) return;
-    if (workflow.defaultDamageType != translate.damageType('healing')) return;
-    if (chris.checkTrait(token.actor, 'di', translate.damageType('healing'))) return;
+    if (workflow.defaultDamageType != translate.healingType('healing')) return;
+    if (chris.checkTrait(token.actor, 'di', translate.healingType('healing'))) return;
     let newHealingTotal = 0;
     let queueSetup = await queue.setup(workflow.uuid, 'beaconOfHope', 351);
     if (!queueSetup) return;
     for (let i = 0; workflow.damageRoll.terms.length > i; i++) {
         let flavor = workflow.damageRoll.terms[i].flavor;
         let isDeterministic = workflow.damageRoll.terms[i].isDeterministic;
-        if (flavor.toLowerCase() === translate.damageType('healing') && !isDeterministic) {
+        if (flavor.toLowerCase() === translate.healingType('healing') && !isDeterministic) {
             newHealingTotal += workflow.damageRoll.terms[i].faces * workflow.damageRoll.terms[i].results.length;
         } else {
             if (!isNaN(workflow.damageRoll.terms[i].total)) {
@@ -21,7 +21,7 @@ export async function beaconOfHope(token, {item, workflow, ditem}) {
             }
         }
     }
-    if (chris.checkTrait(token.actor, 'dr', translate.damageType('healing'))) newHealingTotal = Math.floor(newHealingTotal / 2);
+    if (chris.checkTrait(token.actor, 'dr', translate.healingType('healing'))) newHealingTotal = Math.floor(newHealingTotal / 2);
     let maxHP = token.actor.system.attributes.hp.max;
     ditem.hpDamage = -Math.clamped(newHealingTotal, 0, maxHP - ditem.oldHP);
     ditem.newHP = Math.clamped(ditem.oldHP + newHealingTotal, 0, maxHP);
--- scripts/translations.js
+++ scripts/translations.js
@@ -1,10 +1,14 @@
 function damageType(type) {
     return CONFIG.DND5E.damageTypes[type].toLowerCase();
 }
+function healingType(type) {
+    return CONFIG.DND5E.healingTypes[type].toLowerCase();
+}
 function skills(skill) {
     return CONFIG.DND5E.skills[skill].label;
 }
 export let translate = {
     'damageType': damageType,
+    'healingType': healingType,
     'skills': skills
 }
