3a84b3e1c348340185f783ddd0204b5d531e0f7c
Chris <chrisk123999@gmail.com>
2023-10-24 15:49:37 -0700

    Update charmAnimalsAndPlants.js

--- scripts/macros/classFeatures/cleric/natureDomain/charmAnimalsAndPlants.js
+++ scripts/macros/classFeatures/cleric/natureDomain/charmAnimalsAndPlants.js
@@ -1,6 +1,5 @@
 import {chris} from '../../../../helperFunctions.js';
 export async function charmAnimalsAndPlants({speaker, actor, token, character, item, args, scope, workflow}) {
     let validTargets = Array.from(workflow.targets).filter(i => ['beast', 'plant'].includes(chris.raceOrType(i.actor))).map(i => i.id);
-    console.log(validTargets);
     chris.updateTargets(validTargets);
 }
