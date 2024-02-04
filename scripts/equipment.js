35684bdca422fb308fe3464eadb3470c21e31f51
4535992 <tentimarco0@gmail.com>
2024-02-04 13:21:46 +0100

    Bug fix wrong behaviour on the itemFeatures code ( i think ?) (#186)
    * fix document cannot be retrieved asynchronisouly
    
    * Bug fix wrong behaviour on the itemFeatures code ( i think ?)

--- scripts/equipment.js
+++ scripts/equipment.js
@@ -1,37 +1,50 @@
 import {chris} from './helperFunctions.js';
 export async function itemFeatures(item, updates, options, id) {
-    if (!item.actor) return;
+    if (!item?.actor) return;
     let chrisFeatures = item.flags?.['chris-premades']?.equipment;
     if (!chrisFeatures) return;
     let currentlyEquipped = updates.system?.equipped ?? item.system.equipped;
     let currentlyAttuned = updates.system?.attunement ?? item.system.attunement;
-    if (!currentlyAttuned && !currentlyEquipped) return;
+    // Current Chris Premades Equipment from this item
+    let currentItems = item.actor.items.filter(i => i.flags?.['chris-premades']?.equipmentFeature?.id === item.id) || [];
+    if (!currentlyAttuned && !currentlyEquipped && currentItems.length === 0) return;
     let previouslyEquipped = item.system?.equipped;
     let previouslyAttuned = item.system?.attunement;
     let previousState = previouslyEquipped && (previouslyAttuned === 0 || previouslyAttuned === 2);
     let currentState = currentlyEquipped && (currentlyAttuned === 0 || currentlyAttuned === 2);
+
+    let currentSource = foundry.utils.getProperty(item, "system.source");
+    let currentSourceUuid = foundry.utils.getProperty(item, "flags.core.sourceId");
+
     if (previousState === currentState) return;
     if (previousState && !currentState) {
-        let removeItems = item.actor.items.filter(i => i.flags?.['chris-premades']?.equipmentFeature?.id === item.id);
+        let removeItems = currentItems;
         if (removeItems.length === 0) return;
         let updates = {};
         for (let i of removeItems) setProperty(updates, 'flags.chris-premades.equipment.uses.' + i.name, i.system.uses);
         await item.update(updates);
         await item.actor.deleteEmbeddedDocuments('Item', removeItems.map(i => i.id));
     } else if (!previousState && currentState) {
-        if (!chrisFeatures.items) return;
+        let addItemsUniqueNames = currentItems
+            .map(i => {return i.flags?.['chris-premades']?.equipmentFeature?.uniqueName});
         let items = [];
         for (let i of chrisFeatures.items) {
+            // Do not add the same feature more than once on the same actor
+            // at least based on the uniqueName property
+            if(addItemsUniqueNames.includes(i.uniqueName)) continue;
             let itemData = await chris.getItemFromCompendium(i.key, i.name);
             if (!itemData) continue;
             itemData.system.description.value = chris.getItemDescription('CPR - Descriptions', i.name);
             let uses = getProperty(item, 'flags.chris-premades.equipment.uses.' + i.name);
             if (uses) setProperty(itemData, 'system.uses', uses);
             setProperty(itemData, 'flags.chris-premades.equipmentFeature.id', item.id);
+            setProperty(itemData, 'flags.core.sourceId', item.uuid || currentSourceUuid);
+            setProperty(itemData, 'system.source.custom', item.name);
             if (i.uniqueName) setProperty(itemData, 'flags.chris-premades.equipmentFeature.uniqueName', i.uniqueName);
+            else console.warn("chris-premades | You must set a uniqueName for the Chris Premades Equipment", i, item);
             items.push(itemData);
         }
-        if (item.length === 0) return;
+        if (items.length === 0) return;
         await item.actor.createEmbeddedDocuments('Item', items);
     }
 }
