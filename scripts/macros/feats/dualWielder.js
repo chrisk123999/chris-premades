30a9de320e3acaa678343c711709cb2a8b216934
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2024-02-18 17:09:28 -0500

    Update dualWielder.js (#194)
    In D&D 3.0.x, not every item type has `system.type`, so guard against that being null.

--- scripts/macros/feats/dualWielder.js
+++ scripts/macros/feats/dualWielder.js
@@ -6,7 +6,7 @@ export async function dualWielder(item, updates, options, userId) {
     let effect = chris.findEffect(actor, 'Dual Wielder');
     if (!effect) return;
     let items = actor.items.filter(i => i.type==='weapon' && i.system.equipped);
-    let shields = actor.items.filter(i => i.system.type.value === 'shield' && i.system.equipped);
+    let shields = actor.items.filter(i => i.system.type?.value === 'shield' && i.system.equipped);
     if (shields.length) {
         if (!effect.disabled) await effect.update({'disabled': true});
         return;
