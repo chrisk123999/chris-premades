38ff9c8419fae67fc8279f1fcbc12583244ee4c8
Chris <chrisk123999@gmail.com>
2024-03-23 04:05:54 -0700

    Sync

--- scripts/macros/classFeatures/ranger/gloomStalker/umbralSight.js
+++ scripts/macros/classFeatures/ranger/gloomStalker/umbralSight.js
@@ -1,4 +1,5 @@
 import {chris} from '../../../../helperFunctions.js';
+import {queue} from '../../../../utility/queue.js';
 export async function umbralSight({speaker, actor, token, character, item, args, scope, workflow}) {
     if (workflow.targets.size != 1 || !workflow.token) return;
     let validTypes = ['dark', 'dim'];
@@ -7,8 +8,14 @@ export async function umbralSight({speaker, actor, token, character, item, args,
     let distance = chris.getDistance(workflow.token, targetToken);
     let targetSenses = targetToken.actor.system.attributes.senses;
     if ((!targetSenses.darkvision) || (targetSenses.tremorsense >= distance) || (targetSenses.blindsight >= distance) || (targetSenses.truesight >= distance)) return;
+    let queueSetup = await queue.setup(workflow.item.uuid, 'umbralSight', 150);
+    if (!queueSetup) return;
     let feature = chris.getItem(workflow.actor, 'Umbral Sight');
-    if (!feature) return;
+    if (!feature) {
+        queue.remove(workflow.item.uuid);
+        return;
+    }
     workflow.advantage= true;
     workflow.attackAdvAttribution.add('Advantage: ' + feature.name);
+    queue.remove(workflow.item.uuid);
 }
--- scripts/macros/monsterFeatures/shadowDemon/lightSensitivity.js
+++ scripts/macros/monsterFeatures/shadowDemon/lightSensitivity.js
@@ -1,11 +1,15 @@
 import {chris} from '../../../helperFunctions.js';
+import {queue} from '../../../utility/queue.js';
 async function attack({speaker, actor, token, character, item, args, scope, workflow}) {
     let feature = chris.getItem(workflow.actor, 'Light Sensitivity');
     if (!feature || !workflow.token) return;
     if (!chris.getConfiguration(feature, 'enabled')) return;
     if (chris.checkLight(workflow.token) === 'bright') {
+        let queueSetup = await queue.setup(workflow.item.uuid, 'lightSensitivity', 150);
+        if (!queueSetup) return;
         workflow.disadvantage = true;
         workflow.attackAdvAttribution.add('Disadvantage: ' + feature.name);
+        queue.remove(workflow.item.uuid);
     }
 }
 function perception(skillId, options) {
