26e64ed83137a116cb479196f95a004f59439883
Chris <chrisk123999@gmail.com>
2024-05-21 02:33:47 -0700

    Sync

--- scripts/effectInterface.js
+++ scripts/effectInterface.js
@@ -1,4 +1,5 @@
 let effectItem;
+let effectCollection;
 async function startup() {
     effectItem = game.items.find(i => i.flags['chris-premades']?.effectInterface);
     if (!effectItem) {
@@ -14,17 +15,22 @@ async function startup() {
         };
         effectItem = await Item.create(itemData);
     }
+    let effects = effectItem.effects.contents;
+    effectCollection = new CPREffects(effects);
     Hooks.on('changeSidebarTab', (directory) => {
         // eslint-disable-next-line no-undef
         if (!(directory instanceof ItemDirectory)) return;
         let html = directory.element;
         let li = html.find('li[data-document-id="' + effectItem.id + '"]');
-        li.remove();
+        //li.remove();
     });
 }
 // eslint-disable-next-line no-undef
 class CPREffects extends WorldCollection {
     static documentName = 'ActiveEffect';
+    static _sortStandard(a, b) {
+        return a.name.localeCompare(b.name);
+    }
 }
 class CPREffectInterface extends DocumentDirectory {
     static documentName = 'Effect';
@@ -36,7 +42,29 @@ class CPREffectInterface extends DocumentDirectory {
         });
     }
     _getEntryContextOptions() {
-        let options = super._getEntryContextOptions();
+        let options = [
+            {
+                'callback': async (header) => {
+                    console.log(header);
+                    let documentId = header[0].dataset.documentId;
+                    console.log(documentId);
+                    let document = effectItem.effects.get(documentId);
+                    console.log(document);
+                    if (document) await document.delete();
+                },
+                'condition': () => game.user.isGM,
+                'icon': '<i class="fas fa-trash"></i>',
+                'name': 'SIDEBAR.Delete'
+            },
+            {
+                'callback': (header) => {
+                    console.log(header);
+                },
+                'condition': () => game.user.isGM,
+                'icon': '<i class="far fa-copy"></i>',
+                'name': 'SIDEBAR.Duplicate'
+            },
+        ];
         return options;
     }
     _onClickThumbnail(event) {
@@ -47,10 +75,15 @@ class CPREffectInterface extends DocumentDirectory {
         console.log(document);
     }
     static get collection() {
-        console.log(effectItem);
-        let effects = effectItem?.effects?.contents ?? [];
-        console.log(effects);
-        return new CPREffects(effects);
+        let effectItem = game.items.find(i => i.flags['chris-premades']?.effectInterface);
+        if (effectItem) {
+            let effects = effectItem.effects.contents;
+            effectCollection = new CPREffects(effects);
+        } else {
+            effectCollection = new CPREffects([]);
+        }
+        console.log(effectCollection);
+        return effectCollection;
     }
     get tabName() {
         return 'effects';
@@ -61,10 +94,46 @@ class CPREffectInterface extends DocumentDirectory {
     initialize() {
         this.folders = null;
         this.documents = this.collection;
-        //this.collection.initializeTree();
+        console.log(this.collection);
+        this.collection.initializeTree();
     }
     get canCreateEntry() {
         return game.user.isGM;
+    }
+    get canCreateFolder() {
+        return false;
+    }
+    async _onClickEntryName(event) {
+        event.preventDefault();
+        let element = event.currentTarget;
+        let documentId = element.parentElement.dataset.documentId;
+        let document = effectItem.effects.get(documentId);
+        if (document) document.sheet.render(true);
+    }
+    async _onCreateEntry(event) {
+        event.preventDefault();
+        //event.stopPropagation();
+        /*
+        const button = event.currentTarget;
+        const li = button.closest('.directory-item');
+        const data = {folder: li?.dataset?.folderId};
+
+        const cls = 'ActiveEffect';
+        */
+        let effectData = {
+            'name': 'New Effect',
+            'icon': 'icons/svg/aura.svg',
+            'transfer': false
+        };
+        // eslint-disable-next-line no-undef
+        let createdEffect = await ActiveEffect.create(effectData, {'parent': effectItem});
+        await createdEffect.sheet.render(true);
+        console.log(effectCollection);
+        //Make this refresh or update the sidebar somehow?
+    }
+    _onRightClickTab(event) {
+        event.preventDefault();
+        return;
     }
 }
 function effectSidebar(app, html, data) {
