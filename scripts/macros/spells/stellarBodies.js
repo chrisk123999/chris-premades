8e3aec1fb82980a34b4215fe4b059edc08df198a
Michael Roth <michaelhroth624@gmail.com>
2024-06-06 12:53:43 -0400

    fixed up stellar bodies

--- scripts/macros/spells/stellarBodies.js
+++ scripts/macros/spells/stellarBodies.js
@@ -4,11 +4,8 @@ import {translate} from '../../translations.js';
 import {queue} from '../../utility/queue.js';
 async function item({speaker, actor, token, character, item, args, scope, workflow}) {
     let maxStars = Math.floor(workflow.castData.castLevel / 2);
-    let dimLight = (maxStars * 5);
-    console.log(dimLight);
     let featureData = await chris.getItemFromCompendium('chris-premades.CPR Spell Features', 'Stellar Body - Attack', false);
     if (!featureData) return;
-    console.log("got feature");
     featureData.system.description.value = chris.getItemDescription('CPR - Descriptions', 'Stellar Body - Attack');
     async function effectMacro () {
         await warpgate.revert(token.document, 'Stellar Bodies');
@@ -140,7 +137,6 @@ async function update(workflow, targetToken) {
     let effect = chris.findEffect(workflow.token.actor, 'Stellar Bodies');
     if (!effect) return;
     let newStars = (effect.flags['chris-premades']?.spell?.stellarBodies.stars - 1);
-    console.log(newStars);
     if (newStars == 0) {
         await warpgate.revert(workflow.token.document, 'Stellar Bodies');
         Sequencer.EffectManager.endEffects({'name': 'Stellar Bodies', 'object': token});
@@ -148,6 +144,38 @@ async function update(workflow, targetToken) {
     }
 
     let updates = {
+        'changes': [
+            {
+                'key': 'ATL.light.dim',
+                'value': (newStars * 5),
+                'mode': 5,
+                'priority': 20
+            },
+            {
+                'key': 'ATL.light.color',
+                'value': '#ffffff',
+                'mode': 5,
+                'priority': 20
+            },
+            {
+                'key': 'ATL.light.alpha',
+                'value': '0.25',
+                'mode': 5,
+                'priority': 20
+            },
+            {
+                'key': 'ATL.light.animation',
+                'value': '{\'type\': \'starlight\', \'speed\': 1,\'intensity\': 3}',
+                'mode': 5,
+                'priority': 20
+            },
+            {
+                "key": "flags.chris-premades.feature.onHit.stellarBodies",
+                "value": "true",
+                "mode": 5,
+                "priority": 20
+            }
+        ],
         'flags': {
             'chris-premades': {
                 'spell': {
@@ -159,23 +187,19 @@ async function update(workflow, targetToken) {
         }
     }
     await chris.updateEffect(effect, updates);
-    //update to light effect?
 }
 
 async function onHit(workflow, targetToken) {
     if (workflow.hitTargets.size != 1) return;
     if (!constants.meleeAttacks.includes(workflow.item?.system?.actionType)) return;
     let effect = chris.findEffect(targetToken.actor, 'Stellar Bodies');
-    console.log(effect);
     if (!effect) return;
 
     let stars = effect.flags['chris-premades']?.spell?.stellarBodies.stars;
     let damage = `${stars}d8`;
-    console.log(damage);
     let queueSetup = await queue.setup(workflow.uuid, 'stellarBodies', 50);
     if (!queueSetup) return;
     let featureData = await chris.getItemFromCompendium('chris-premades.CPR Spell Features', 'Stellar Bodies - Protect');
-    console.log(featureData);
     if (!featureData) {
         queue.remove(workflow.uuid);
         return;
