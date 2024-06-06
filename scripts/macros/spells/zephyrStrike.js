84a3cb55ee790b4d0434b3e3d0308bd77ad6e361
Michael Roth <michaelhroth624@gmail.com>
2024-06-06 12:45:55 -0400

    fixed up zephyr strike

--- scripts/macros/spells/zephyrStrike.js
+++ scripts/macros/spells/zephyrStrike.js
@@ -2,31 +2,35 @@ import {constants} from '../../constants.js';
 import {queue} from '../../utility/queue.js';
 import {chris} from '../../helperFunctions.js';
 async function damage({speaker, actor, token, character, item, args, scope, workflow}) {
-    console.log("damage");
     if (workflow.hitTargets.size == 0 || !workflow.item) return;
-    console.log(workflow);
-    console.log(workflow.item.system.actionType);
     if (!constants.weaponAttacks.includes(workflow.item.system.actionType)) return;
-    console.log(workflow.item.system.actionType);
-    let effect = chris.getEffects(workflow.actor).find(i => i.flags['chris-premades']?.spell?.zephyrStrikeAdvantage);
-    console.log(effect);
+    let effect = chris.getEffects(workflow.actor).find(i => i.flags['chris-premades']?.spell?.zephyrStrike?.advantage);
     if (!effect) return;
     let queueSetup = await queue.setup(workflow.item.uuid, 'zephyrStrike', 250);
     if (!queueSetup) return;
     let bonusDamageFormula = '1d8[force]';
     await chris.addToDamageRoll(workflow, bonusDamageFormula);
-
-    await chris.removeEffect(effect);
+    await effect.update({
+        'flags.chris-premades.spell.zephyrStrike.advantage': false
+    });
     queue.remove(workflow.item.uuid);
 }
 async function postAttack({speaker, actor, token, character, item, args, scope, workflow}) {
-    console.log("postAttack")
     if (!constants.weaponAttacks.includes(workflow.item.system.actionType) || workflow.hitTargets.size != 0 || !workflow.item) return;
-    let effect = chris.getEffects(workflow.actor).find(i => i.flags['chris-premades']?.spell?.zephyrStrikeAdvantage);
-    await chris.removeEffect(effect);
+    let effect = chris.getEffects(workflow.actor).find(i => i.flags['chris-premades']?.spell?.zephyrStrike?.advantage);
+    if (!effect) return;
+    await effect.update({
+        'flags.chris-premades.spell.zephyrStrike.advantage': false
+    })
 }
 
-async function buff({speaker, actor, token, character, item, args, scope, workflow}) {
+async function preAttack({speaker, actor, token, character, item, args, scope, workflow}) {
+    let effect = chris.findEffect(workflow.actor, 'Zephyr Strike');
+    if (!effect) return;
+    if (effect.flags?.['chris-premades']?.spell?.zephyrStrike?.used) return;
+    if (!constants.weaponAttacks.includes(workflow.item.system.actionType)) return;
+    let selection = await chris.dialog('Use Zephyr Strike?', constants.yesNo, 'Apply Zephyr Strike to gain advantage & extra damage?');
+    if (!selection) return;
     let movementEffectData = {
         'icon': 'icons/skills/movement/feet-winged-boots-glowing-yellow.webp',
         'origin': workflow.item.uuid,
@@ -51,61 +55,16 @@ async function buff({speaker, actor, token, character, item, args, scope, workfl
         }
     };
     await chris.createEffect(workflow.actor, movementEffectData);
-
-    let advantageEffectData = {
-        'icon': 'icons/skills/targeting/crosshair-pointed-orange.webp',
-        'origin': workflow.item.uuid,
-        'duration': {
-            'turns': 1
-        },
-        'name': 'Zephyr Strike - Advantage',
-        'changes': [
-            {
-                'key': 'flags.midi-qol.onUseMacroName',
-                'mode': 0,
-                'value': 'function.chrisPremades.macros.zephyrStrike.postAttack,postAttackRoll',
-                'priority': 20
-            },
-            {
-                'key': 'flags.midi-qol.onUseMacroName',
-                'mode': 0,
-                'value': 'function.chrisPremades.macros.zephyrStrike.damage,postDamageRoll',
-                'priority': 20
-            },
-            {
-                'key': "flags.midi-qol.advantage.attack.mwak",
-                'mode': 0,
-                'value': 1,
-                'priority': 0
-            },
-            {
-                'key': "flags.midi-qol.advantage.attack.rwak",
-                'mode': 0,
-                'value': 1,
-                'priority': 0
-            }
-        ],
-        'flags': {
-            'chris-premades': {
-                'spell': {
-                    'zephyrStrikeAdvantage': {
-                        'active': true
-                    }
-                }
-            }
+    workflow.advantage = true;
+    await effect.update({
+        'flags.chris-premades.spell.zephyrStrike': {
+            'used': true,
+            'advantage': true
         }
-    };
-    await chris.createEffect(workflow.actor, advantageEffectData);
+    });
 }
 
 async function item({speaker, actor, token, character, item, args, scope, workflow}) {
-    let featureData = await chris.getItemFromCompendium('chris-premades.CPR Spell Features', 'Zephyr Strike - Advantage', false);
-    if (!featureData) return;
-    featureData.system.description.value = chris.getItemDescription('CPR - Descriptions', 'Zephyr Strike - Advantage');
-    console.log(featureData);
-    async function effectMacro () {
-        await warpgate.revert(token.document, 'Zephyr Strike');
-    }
     let effectData = {
         'name': workflow.item.name,
         'icon': workflow.item.img,
@@ -113,45 +72,44 @@ async function item({speaker, actor, token, character, item, args, scope, workfl
         'duration': {
             'seconds': 60
         },
+        'changes': [
+            {
+                'key': 'flags.midi-qol.onUseMacroName',
+                'mode': 0,
+                'value': 'function.chrisPremades.macros.zephyrStrike.preAttack,preAttackRoll',
+                'priority': 20
+            }, {
+                'key': 'flags.midi-qol.onUseMacroName',
+                'mode': 0,
+                'value': 'function.chrisPremades.macros.zephyrStrike.postAttack,postAttackRoll',
+                'priority': 20
+            }, {
+                'key': 'flags.midi-qol.onUseMacroName',
+                'mode': 0,
+                'value': 'function.chrisPremades.macros.zephyrStrike.damage,postDamageRoll',
+                'priority': 20
+            }
+        ],
         'flags': {
-            'effectmacro': {
-                'onDelete': {
-                    'script': chris.functionToString(effectMacro)
-                }
-            },
             'chris-premades': {
                 'spell': {
                     'zephyrStrike': {
-                        'used': false
+                        'used': false,
+                        'advantage': false
                     }
                 },
                 'vae': {
-                    'button': featureData.name
+                    'button': workflow.item.name
                 }
             }
         }
     };
-    let updates = {
-        'embedded': {
-            'Item': {
-                [featureData.name]: featureData
-            },
-            'ActiveEffect': {
-                [featureData.name]: effectData
-            }
-        }
-    };
-    let options = {
-        'permanent': false,
-        'name': 'Zephyr Strike',
-        'description': effectData.name
-    };
-    await warpgate.mutate(workflow.token.document, updates, {}, options);
+    await chris.createEffect(workflow.actor, effectData, workflow.item);
 }
 
 export let zephyrStrike = {
     'damage': damage,
-    'buff': buff,
+    'preAttack': preAttack,
     'postAttack': postAttack,
     'item': item
 };
