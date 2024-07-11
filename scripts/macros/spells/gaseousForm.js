9b6c2c506603f090273781411d7ff017d132b98f
Michael Roth <michaelhroth624@gmail.com>
2024-07-11 12:28:34 -0400

    sync

---
+++ scripts/macros/spells/gaseousForm.js
@@ -0,0 +1,4 @@
+export let gaseousForm = {
+    name: 'Gaseous Form',
+    version: '0.12.0'
+};
---
+++ scripts/macros/spells/hailOfThorns.js
@@ -0,0 +1,75 @@
+import {compendiumUtils, constants, effectUtils, errors, genericUtils, itemUtils, tokenUtils, workflowUtils} from '../../utils.js';
+
+async function use({workflow}) {
+    let concentrationEffect = await effectUtils.getConcentrationEffect(workflow.actor, workflow.item);
+    let effectData = {
+        name: workflow.item.name,
+        img: workflow.item.img,
+        origin: workflow.item.uuid,
+        duration: {
+            seconds: 60 * workflow.item.system.duration.value
+        },
+        flags: {
+            'chris-premades': {
+                hailOfThorns: {
+                    castLevel: workflow.castData?.castLevel ?? 1,
+                    dc: itemUtils.getSaveDC(workflow.item)
+                }
+            }
+        }
+    };
+    effectUtils.addMacro(effectData, 'midi.actor', ['hailOfThornsBurst']);
+    await effectUtils.createEffect(workflow.actor, effectData, {concentrationItem: workflow.item, strictlyInterdependent: true, identifier: 'hailOfThornsBurst'});
+    if (concentrationEffect) genericUtils.update(concentrationEffect, {'duration.seconds': effectData.duration.seconds});
+}
+async function late({workflow}) {
+    if (workflow.hitTargets.size !== 1) return;
+    if (workflow.item?.system?.actionType !== 'rwak') return;
+    let effect = effectUtils.getEffectByIdentifier(workflow.actor, 'hailOfThornsBurst');
+    if (!effect) return;
+    let featureData = await compendiumUtils.getItemFromCompendium(constants.featurePacks.spellFeatures, 'Hail of Thorns: Burst', {getDescription: true, translate: 'CHRISPREMADES.macros.hailOfThorns.burst', object: true});
+    if (!featureData) {
+        errors.missingPackItem();
+        return;
+    }
+    let damageDice = Math.min(effect.flags['chris-premades']?.hailOfThorns?.castLevel ?? 1, 6);
+    featureData.system.damage.parts = [
+        [
+            damageDice + 'd10[piercing]',
+            'piercing'
+        ]
+    ];
+    let saveDC = effect.flags['chris-premades']?.hailOfThorns?.dc ?? 10;
+    featureData.system.save.dc = saveDC;
+    // TODO: school? Why
+    let targetToken = workflow.targets.first();
+    let allTargets = tokenUtils.findNearby(targetToken, 5).concat(targetToken);
+    await workflowUtils.syntheticItemDataRoll(featureData, workflow.actor, allTargets);
+    await genericUtils.remove(effect);
+}
+export let hailOfThorns = {
+    name: 'Hail of Thorns',
+    version: '0.12.0',
+    midi: {
+        item: [
+            {
+                pass: 'rollFinished',
+                macro: use,
+                priority: 50
+            }
+        ]
+    }
+};
+export let hailOfThornsBurst = {
+    name: 'Hail of Thorns: Burst',
+    version: hailOfThorns.version,
+    midi: {
+        actor: [
+            {
+                pass: 'rollFinished',
+                macro: late,
+                priority: 50
+            }
+        ]
+    }
+};
