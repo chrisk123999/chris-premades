6dca327d7942a313b519ea882c2f5b98f7a5670f
Thorryon <103772619+Thorryon@users.noreply.github.com>
2024-07-14 10:31:09 +0200

    Update fallenPuppet.js
    Added 1hp to resurrect the creature

--- scripts/macros/classFeatures/bloodHunter/bloodCurses/fallenPuppet.js
+++ scripts/macros/classFeatures/bloodHunter/bloodCurses/fallenPuppet.js
@@ -25,7 +25,13 @@ export async function fallenPuppet({speaker, actor, token, character, item, args
                 'mode': 2,
                 'priority': 20,
                 'value': '+' + modifier
-            }
+            },
+			{
+				'key': 'system.attributes.hp.value',
+                'mode': 4,
+                'priority': 20,
+                'value': 1
+			}
         ]
     };
     await chris.updateEffect(effect, updates);
