43f6a1aa518ca28590179cf76ae9f80df2c4d271
Michael Roth <michaelhroth624@gmail.com>
2024-07-24 16:01:14 -0400

    Sync

---
+++ scripts/macros/spells/crusadersMantle.js
@@ -0,0 +1,4 @@
+export let crusadersMantle = {
+    name: 'Crusader\'s Mantle',
+    version: '0.12.0'
+};
---
+++ scripts/macros/spells/passWithoutTrace.js
@@ -0,0 +1,4 @@
+export let passWithoutTrace = {
+    name: 'passWithoutTrace',
+    version: '0.12.0'
+};
