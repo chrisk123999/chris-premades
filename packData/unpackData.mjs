0fe2c3ac76982cd8f4d8a5d5d441cec0b0272f8e
Michael Roth <michaelhroth624@gmail.com>
2024-08-03 13:54:15 -0400

    ignore modifiedTime

--- packData/unpackData.mjs
+++ packData/unpackData.mjs
@@ -21,8 +21,8 @@ let actorPacks = [
     'cpr-summons'
 ];
 for (let i of itemPacks) {
-    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Item'});
+    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Item', transformEntry: (entry) => {delete entry._stats.modifiedTime;}});
 }
 for (let i of actorPacks) {
-    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Actor'});
+    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Actor', transformEntry: (entry) => {delete entry._stats.modifiedTime;}});
 }
