78037f072f513d0077be7ad13ef3e1dfdefe66cc
chrisk123999 <autumnrrosser@gmail.com>
2024-08-19 16:14:33 -0700

    0.12.24

--- packData/unpackData.mjs
+++ packData/unpackData.mjs
@@ -22,8 +22,8 @@ let actorPacks = [
     'cpr-summons'
 ];
 for (let i of itemPacks) {
-    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Item', transformEntry: (entry) => {delete entry._stats; delete entry.sort;}});
+    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Item', transformEntry: (entry) => {delete entry._stats; delete entry.sort; delete entry.ownership;}});
 }
 for (let i of actorPacks) {
-    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Actor', transformEntry: (entry) => {delete entry._stats; delete entry.sort;}});
+    await extractPack('packs/' + i, 'packData/' + i, {'log': true, 'documentType': 'Actor', transformEntry: (entry) => {delete entry._stats; delete entry.sort; delete entry.ownership;}});
 }
