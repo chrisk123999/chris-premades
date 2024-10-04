984e05f9a1f9ea6d86c95258eff0c6c82119b748
Michael Roth <michaelhroth624@gmail.com>
2024-10-04 14:03:14 -0400

    remove redundant ddbi martial adept maneuvers

--- scripts/macros/feats/martialAdept.js
+++ scripts/macros/feats/martialAdept.js
@@ -17,5 +17,34 @@ export let martialAdept = {
                 priority: 50
             }
         ]
+    },
+    ddbi: {
+        removedItems: {
+            'Martial Adept': [
+                'Martial Adept: Ambush',
+                'Martial Adept: Bait and Switch',
+                'Martial Adept: Brace',
+                'Martial Adept: Commander\'s Strike',
+                'Martial Adept: Commanding Presence',
+                'Martial Adept: Disarming Attack',
+                'Martial Adept: Distracting Strike',
+                'Martial Adept: Evasive Footwork',
+                'Martial Adept: Feinting Attack',
+                'Martial Adept: Goading Attack',
+                'Martial Adept: Grappling Strike',
+                'Martial Adept: Lunging Attack',
+                'Martial Adept: Maneuvering Attack',
+                'Martial Adept: Menacing Attack',
+                'Martial Adept: Parry',
+                'Martial Adept: Precision Attack',
+                'Martial Adept: Pushing Attack',
+                'Martial Adept: Quick Toss',
+                'Martial Adept: Rally',
+                'Martial Adept: Riposte',
+                'Martial Adept: Sweeping Attack',
+                'Martial Adept: Tactical Assessment',
+                'Martial Adept: Trip Attack'
+            ]
+        }
     }
 };
