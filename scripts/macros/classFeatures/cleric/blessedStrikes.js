37234a0f561d22cfb4ce6d79bd91e7f4055fde43
chrisk123999 <chrisk123999@gmail.com>
2024-11-10 18:03:39 -0800

    Blessed Strikes Change

--- scripts/macros/classFeatures/cleric/blessedStrikes.js
+++ scripts/macros/classFeatures/cleric/blessedStrikes.js
@@ -1,5 +1,4 @@
 import {combatUtils, constants, dialogUtils, genericUtils, itemUtils, workflowUtils} from '../../../utils.js';
-
 async function damage({trigger: {entity: item}, workflow}) {
     if (!workflow.hitTargets.size || !workflow.damageRoll) return;
     let itemType = workflow.item.type;
@@ -10,6 +9,7 @@ async function damage({trigger: {entity: item}, workflow}) {
     let damageType = itemUtils.getConfig(item, 'damageType');
     await workflowUtils.bonusDamage(workflow, '1d8[radiant]', {damageType});
     if (combatUtils.inCombat()) await genericUtils.setFlag(item, 'chris-premades', 'blessedStrikes.used', true);
+    await item.use();
 }
 async function clearUsed({trigger: {entity: item}}) {
     await genericUtils.setFlag(item, 'chris-premades', 'blessedStrikes.used', false);
