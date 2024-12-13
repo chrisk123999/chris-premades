04b60d1d3075bba16e34fc6ca8066fb460af26a2
aljames-arctic <bakanabaka.vtt@gmail.com>
2024-12-13 09:40:02 -0600

    Cap pull distance at the distance between tokens

--- scripts/macros/monsterFeatures/generic/autoPush.js
+++ scripts/macros/monsterFeatures/generic/autoPush.js
@@ -1,17 +1,22 @@
-import {itemUtils, tokenUtils} from '../../../utils.js';
+import {itemUtils, genericUtils, tokenUtils} from '../../../utils.js';
 async function use({trigger, workflow}) {
     if (!workflow.token || !workflow.targets.size) return;
     let config = itemUtils.getGenericFeatureConfig(workflow.item, 'autoPush');
     if (isNaN(Number(config.distance))) return;
     workflow.targets.forEach(token => {
         if (config.failed && !workflow.failedSaves.has(token)) return;
-        tokenUtils.pushToken(workflow.token, token, Number(config.distance));
+        let distance = Number(config.distance);
+        if (distance < 0) {
+            let distanceBetween = tokenUtils.getDistance(workflow.token, token, {wallsBlock: true, checkCover: genericUtils.getCPRSetting('movementPerformance') === 3});
+            distance = Math.max(distance, -distanceBetween);
+        }
+        tokenUtils.pushToken(workflow.token, token, distance);
     });
 }
 export let autoPush = {
     name: 'Auto Push',
     translation: 'CHRISPREMADES.Macros.AutoPush.Name',
-    version: '1.0.45',
+    version: '1.0.50',
     midi: {
         item: [
             {
