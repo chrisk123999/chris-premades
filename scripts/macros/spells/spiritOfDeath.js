76c98003f3e67ed4a33727bd59e13bcf6b009654
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2024-12-15 18:07:22 -0500

    Update spiritOfDeath.js

--- scripts/macros/spells/spiritOfDeath.js
+++ scripts/macros/spells/spiritOfDeath.js
@@ -169,7 +169,6 @@ export let spiritOfDeath = {
             category: 'summons'
         },
         {
-        {
             value: 'animation',
             label: 'CHRISPREMADES.Config.SpecificAnimation',
             type: 'select',
