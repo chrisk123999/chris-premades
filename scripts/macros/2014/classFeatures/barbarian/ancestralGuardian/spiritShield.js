9beadcc28baa948b4dad6cc4dad6e6aa3e1bb20c
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2025-02-23 23:52:24 -0500

    Saga tympana spirit shield (#368)
    * Create spiritShield.js
    
    * Update spiritShield.js
    
    * Update legacyMacros.js
    
    * Update en.json
    
    * Update spiritShield.js
    
    * Update en.json
    
    * Update spiritShield.js
    
    * Update spiritShield.js
    
    * Update spiritShield.js
    
    Vengeful Ancestors.

---
+++ scripts/macros/2014/classFeatures/barbarian/ancestralGuardian/spiritShield.js
@@ -0,0 +1,92 @@
+import {actorUtils, dialogUtils, effectUtils, genericUtils, itemUtils, socketUtils, tokenUtils, workflowUtils} from '../../../../../utils.js';
+async function shieldHelper(token, sourceToken, targetToken, ditem) {
+    if (actorUtils.hasUsedReaction(token.actor)) return;
+    let rageEffect = effectUtils.getEffectByIdentifier(token.actor, 'rage');
+    if (!rageEffect) return;
+    let spiritShield = itemUtils.getItemByIdentifier(token.actor, 'spiritShield');
+    if (!spiritShield) return;
+    let selection = await dialogUtils.confirm(spiritShield.name, genericUtils.format('CHRISPREMADES.Macros.SpiritShield.Damage', {item: spiritShield.name, name: targetToken.document.name}, {userId: socketUtils.firstOwner(token.actor, true)}));
+    if (!selection) return;
+    let result = await workflowUtils.syntheticItemRoll(spiritShield, [token], {consumeResources: true, userId: socketUtils.firstOwner(token.actor, true)});
+    workflowUtils.modifyDamageAppliedFlat(ditem, result.damageRolls[0].total);
+    let vengefulAncestors = itemUtils.getItemByIdentifier(token.actor, 'vengefulAncestors');
+    if (vengefulAncestors) {
+        workflowUtils.applyDamage([sourceToken], result.damageRolls[0].total, 'force');
+    }
+    return true;
+}
+async function damageApplication({trigger: {sourceToken, targetToken}, ditem}) {
+    let nearbyTokens = tokenUtils.findNearby(targetToken, 30, 'ally', {includeIncapacitated: false, includeToken: true});
+    for (let i of nearbyTokens) {
+        let shielded = await shieldHelper(i, sourceToken, targetToken, ditem);
+        if (shielded) break;
+    }
+}
+export let spiritShield = {
+    name: 'Spirit Shield',
+    version: '1.2.6',
+    rules: 'legacy',
+    midi: {
+        actor: [
+            {
+                pass: 'sceneApplyDamage',
+                macro: damageApplication,
+                priority: 250
+            }
+        ]
+    },
+    config: [
+        {
+            value: 'classIdentifier',
+            label: 'CHRISPREMADES.Config.ClassIdentifier',
+            type: 'text',
+            default: 'barbarian',
+            category: 'homebrew',
+            homebrew: true
+        },
+        {
+            value: 'scaleIdentifier',
+            label: 'CHRISPREMADES.Config.ScaleIdentifier',
+            type: 'text',
+            default: 'spirit-shield',
+            category: 'homebrew',
+            homebrew: true
+        },
+    ],
+    scales: [
+        {
+            classIdentifier: 'classIdentifier',
+            scaleIdentifier: 'scaleIdentifier',
+            data: {
+                type: 'ScaleValue',
+                configuration: {
+                    distance: {
+                        units: ''
+                    },
+                    identifier: 'spirit-shield',
+                    type: 'dice',
+                    scale: {
+                        6: {
+                            number: 2,
+                            faces: 6,
+                            modifiers: []
+                        },
+                        10: {
+                            number: 3,
+                            faces: 6,
+                            modifiers: []
+                        },
+                        14: {
+                            number: 4,
+                            faces: 6,
+                            modifiers: []
+                        }
+                    }
+                },
+                value: {},
+                title: 'Spirit Shield',
+                icon: null
+            }
+        }
+    ]
+};
