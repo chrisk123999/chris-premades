746eb7f5f08a6a700fd02cd5a79f1ac08b2d1bff
chrisk123999 <chrisk123999@gmail.com>
2025-03-16 14:28:19 -0700

    Grease Fix

--- scripts/extensions/template.js
+++ scripts/extensions/template.js
@@ -1,9 +1,15 @@
+import {custom} from '../events/custom.js';
 import {genericUtils} from '../utils.js';
 function rules(template, updates, options, userId) {
     if (updates.flags['chris-premades']?.rules || !updates.flags.dnd5e?.item) return;
     let item = fromUuidSync(updates.flags.dnd5e.item);
     if (!item) return;
     let rules = genericUtils.getRules(item);
+    let identifier = genericUtils.getIdentifier(item);
+    if (identifier) {
+        let macro = custom.getMacro(identifier, rules);
+        if (macro?.legacyTemplate) rules = 'legacy';
+    }
     template.updateSource({'flags.chris-premades.rules': rules});
 }
 export let template = {
--- scripts/macros/2024/spells/grease.js
+++ scripts/macros/2024/spells/grease.js
@@ -2,5 +2,6 @@ import {grease as greaseLegacy} from '../../../legacyMacros.js';
 export let grease = {
     ...greaseLegacy,
     version: '1.2.22',
-    rules: 'modern'
+    rules: 'modern',
+    legacyTemplate: true
 };
