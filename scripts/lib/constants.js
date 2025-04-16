8e6f1c8ed20e03a2d332007e8870aaa234367118
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2025-04-16 00:59:53 -0400

    Update constants.js
    This should play nicer with translated abbreviations.

--- scripts/lib/constants.js
+++ scripts/lib/constants.js
@@ -137,7 +137,7 @@ const damageTypeOptions = () => Object.entries(CONFIG.DND5E.damageTypes).map(i =
 const creatureTypeOptions = () => Object.entries(CONFIG.DND5E.creatureTypes).map(i => ({label: i[1].label, value: i[0]}));
 const actorCompendiumPacks = () => [{label: '', value: ''}, ...game.packs.filter(i => i.documentName === 'Actor').map(i => ({label: i.metadata.label, value: i.metadata.id}))];
 const itemCompendiumPacks = () => [{label: '', value: ''}, ...game.packs.filter(i => i.documentName === 'Item').map(i => ({label: i.metadata.label, value: i.metadata.id}))];
-const abilityOptions = () => Object.values(CONFIG.DND5E.abilities).map(i => ({label: i.label, value: i.abbreviation}));
+const abilityOptions = () => Object.entries(CONFIG.DND5E.abilities).map(i => ({label: i[1].label, value: i[0]}));
 const healingTypeOptions = () => Object.entries(CONFIG.DND5E.healingTypes).map(i => ({label: i[1].label, value: i[0]}));
 const statusOptions = () => CONFIG.statusEffects.map(i => ({label: i.name, value: i.id}));
 const skillOptions = () => Object.entries(CONFIG.DND5E.skills).map(i => ({label: i[1].label, value: i[0]}));
