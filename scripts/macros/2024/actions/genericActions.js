a2c226e16d1730eb8a4bbacd4c173eecbf20bc88
Tyler <3deathsaves@gmail.com>
2025-11-18 19:20:15 -0700

    Add references to rules corresponding to generic actions

--- scripts/macros/2024/actions/genericActions.js
+++ scripts/macros/2024/actions/genericActions.js
@@ -1,4 +1,17 @@
 import {constants, dialogUtils, genericUtils, itemUtils, workflowUtils} from '../../../utils.js';
+
+// Custom mapping for action names that don't match CONFIG.DND5E.rules slugification
+const ACTION_RULE_MAPPING = {
+    'Check Cover': 'cover',
+    'Fall': 'falling',
+    'Jump': 'jumping',
+    'Knock Out': 'knockingacreatureout',
+    'Squeeze': 'squeezing',
+    'Stabilize': 'stabilizing',
+    'Suffocation': 'suffocating',
+    'Underwater': 'underwatercombat'
+};
+
 async function use({trigger, workflow}) {
     let rules = itemUtils.getConfig(workflow.item, 'rules');
     if (rules === 'system') rules = genericUtils.getRules(workflow.item);
@@ -11,7 +24,16 @@ async function use({trigger, workflow}) {
     if (!circleCast) filter.push('Circle Cast');
     let documents = index.contents;
     documents = documents.filter(i => !filter.includes(i.name));
-    let selection = await dialogUtils.selectDocumentDialog(workflow.item.name, 'CHRISPREMADES.Macros.GenericActions.Select', documents, {sortAlphabetical: true});
+    // Add reference tooltips from CONFIG.DND5E.rules
+    documents = documents.map(i => {
+        let wrappedDoc = genericUtils.duplicate(i);
+        // Check custom mapping first, then fall back to slugification
+        const ruleKey = ACTION_RULE_MAPPING[i.name] ?? i.name.toLowerCase().replace(/\s+/g, '');
+        const reference = CONFIG.DND5E.rules?.[ruleKey];
+        if (reference) wrappedDoc.reference = reference;
+        return wrappedDoc;
+    });
+    let selection = await dialogUtils.selectDocumentDialog(workflow.item.name, 'CHRISPREMADES.Macros.GenericActions.Select', documents, {sortAlphabetical: true, displayReference: true});
     if (!selection) return;
     let documentData = genericUtils.duplicate(selection.toObject());
     documentData.system.description.value = itemUtils.getItemDescription(documentData.name);
