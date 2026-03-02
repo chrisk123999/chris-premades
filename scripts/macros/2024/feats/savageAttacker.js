c05226ce2ac940bad2c07ab8a3d19aa1fe9d9b3a
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2026-03-02 16:41:30 -0500

    Update savageAttacker.js
    Revert the previous attempt to make this more concise. It turns out there were REASONS I originally did it all damage roll by damage roll instead of just treating all of them as one.

--- scripts/macros/2024/feats/savageAttacker.js
+++ scripts/macros/2024/feats/savageAttacker.js
@@ -8,10 +8,20 @@ async function attack({trigger: {entity: item}, workflow}) {
 }
 async function damage({trigger: {entity: item}, workflow}) {
     if (!workflow?.['chris-premades']?.savageAttacker) return;
-    let oldRoll = workflow.damageRoll;
-    let newRoll = await rollUtils.damageRoll(oldRoll.formula, workflow.activity, oldRoll.options);
-    if (oldRoll.total >= newRoll.total) return;
-    await workflow.setDamageRolls([newRoll]);
+    let numWeaponDamageRolls = workflow.activity.damage.parts.length;
+    let trueOld = [];
+    let trueNew = [];
+    for (let i = 0; i < numWeaponDamageRolls; i++) {
+        let currRoll = workflow.damageRolls[i];
+        trueOld.push(currRoll);
+        trueNew.push(await new CONFIG.Dice.DamageRoll(currRoll.formula, currRoll.data, currRoll.options).evaluate());
+    }
+    if (trueOld.reduce((acc, i) => acc + i.total, 0) >= trueNew.reduce((acc, i) => acc + i.total, 0)) return;
+    let newDamageRolls = workflow.damageRolls;
+    for (let i = 0; i < numWeaponDamageRolls; i++) {
+        newDamageRolls[i] = trueNew[i];
+    }
+    await workflow.setDamageRolls(newDamageRolls);
 }
 async function combatEnd({trigger: {entity: item}}) {
     await combatUtils.setTurnCheck(item, 'savageAttacker', true);
