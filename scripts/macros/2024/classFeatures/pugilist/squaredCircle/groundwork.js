ce9348af605f261e4cc49a205772b40d6ad4a356
bacon-nugget <122384608+bacon-nugget@users.noreply.github.com>
2026-03-04 00:38:45 -0500

    Bugfix - Groundwork
    noted - combat events run on GM client

--- scripts/macros/2024/classFeatures/pugilist/squaredCircle/groundwork.js
+++ scripts/macros/2024/classFeatures/pugilist/squaredCircle/groundwork.js
@@ -1,4 +1,4 @@
-import {activityUtils, dialogUtils, constants, effectUtils, genericUtils, itemUtils, rollUtils, workflowUtils} from '../../../../../utils.js';
+import {activityUtils, dialogUtils, constants, effectUtils, genericUtils, itemUtils, rollUtils, socketUtils, workflowUtils} from '../../../../../utils.js';
 
 async function compressionLock({trigger: {entity: item, token}, workflow}) {
     let grapplingEffects = effectUtils.getAllEffectsByIdentifier(token.actor, 'grappling');
@@ -7,11 +7,12 @@ async function compressionLock({trigger: {entity: item, token}, workflow}) {
     let feature = activityUtils.getActivityByIdentifier(item, 'compressionLock', {strict: true});
     if (!feature) return;
     let formula = (await rollUtils.damageRoll(feature.damage.parts[0].formula, token.actor)).formula;
+    let userId = socketUtils.firstOwner(token.actor, true);
     let result = await dialogUtils.selectTargetDialog(
         item.name,
         genericUtils.format('CHRISPREMADES.Macros.Groundwork.CompressionLock', {formula}),
         potentialTargets,
-        {type: 'multiple', maxAmount:potentialTargets.length}
+        {type: 'multiple', maxAmount: potentialTargets.length, userId}
     );
     if (!result) return;
     let [targets, filterDefeated] = result;
