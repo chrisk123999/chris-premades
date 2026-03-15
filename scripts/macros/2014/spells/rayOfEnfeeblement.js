7fc6d5b32a50eb21bee17a1f16414a80b27a41da
Chris <chrisk123999@gmail.com>
2026-03-14 20:07:18 -0400

    item -> activity

--- scripts/macros/2014/spells/rayOfEnfeeblement.js
+++ scripts/macros/2014/spells/rayOfEnfeeblement.js
@@ -33,7 +33,7 @@ async function damage({workflow}) {
         if (str.value < dex.value) return;
     }
     workflow.damageRolls = await Promise.all(workflow.damageRolls.map(async damageRoll => {
-        return await new CONFIG.Dice.DamageRoll('floor((' + damageRoll.formula + ') / 2)', workflow.item.getRollData(), damageRoll.options);
+        return await new CONFIG.Dice.DamageRoll('floor((' + damageRoll.formula + ') / 2)', workflow.activity.getRollData(), damageRoll.options);
     }));
     await workflow.setDamageRolls(workflow.damageRolls);
 }
