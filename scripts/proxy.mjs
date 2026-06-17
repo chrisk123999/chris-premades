0be3e2e65f32ffef045e6f566678a7d8e91e81f4
Chris <chrisk123999@gmail.com>
2026-06-17 11:32:43 -0400

    Typing!

--- scripts/proxy.mjs
+++ scripts/proxy.mjs
@@ -28,27 +28,55 @@ function createProxy(targetPath) {
         }
     });
 }
+/** @type {typeof import('cat/scripts/api.mjs').default} */
 export const api = createProxy(['api']);
+/** @type {typeof import('cat/scripts/applications/_module.mjs')} */
 export const applications = createProxy(['applications']);
+/** @type {typeof import('cat/scripts/lib/_module.mjs').Crosshairs} */
 export const Crosshairs = createProxy(['lib', 'Crosshairs']);
+/** @type {typeof import('cat/scripts/lib/_module.mjs').Logging} */
 export const Logging = createProxy(['lib', 'Logging']);
+/** @type {typeof import('cat/scripts/lib/_module.mjs').constants} */
 export const constants = createProxy(['lib', 'constants']);
+/** @type {typeof import('cat/scripts/utilities/activityUtils.mjs').default} */
 export const activityUtils = createProxy(['utils', 'activityUtils']);
+/** @type {typeof import('cat/scripts/utilities/actorUtils.mjs').default} */
 export const actorUtils = createProxy(['utils', 'actorUtils']);
+/** @type {typeof import('cat/scripts/utilities/automationUtils.mjs').default} */
 export const automationUtils = createProxy(['utils', 'automationUtils']);
+/** @type {typeof import('cat/scripts/utilities/combatUtils.mjs').default} */
 export const combatUtils = createProxy(['utils', 'combatUtils']);
+/** @type {typeof import('cat/scripts/utilities/crosshairUtils.mjs').default} */
 export const crosshairUtils = createProxy(['utils', 'crosshairUtils']);
+/** @type {typeof import('cat/scripts/utilities/documentUtils.mjs').default} */
 export const documentUtils = createProxy(['utils', 'documentUtils']);
+/** @type {typeof import('cat/scripts/utilities/effectUtils.mjs').default} */
 export const effectUtils = createProxy(['utils', 'effectUtils']);
+/** @type {typeof import('cat/scripts/utilities/genericUtils.mjs').default} */
 export const genericUtils = createProxy(['utils', 'genericUtils']);
+/** @type {typeof import('cat/scripts/utilities/itemUtils.mjs').default} */
 export const itemUtils = createProxy(['utils', 'itemUtils']);
+/** @type {typeof import('cat/scripts/utilities/queryUtils.mjs').default} */
 export const queryUtils = createProxy(['utils', 'queryUtils']);
+/** @type {typeof import('cat/scripts/utilities/regionUtils.mjs').default} */
 export const regionUtils = createProxy(['utils', 'regionUtils']);
+/** @type {typeof import('cat/scripts/utilities/rollUtils.mjs').default} */
 export const rollUtils = createProxy(['utils', 'rollUtils']);
+/** @type {typeof import('cat/scripts/utilities/sceneUtils.mjs').default} */
 export const sceneUtils = createProxy(['utils', 'sceneUtils']);
+/** @type {typeof import('cat/scripts/utilities/tokenUtils.mjs').default} */
 export const tokenUtils = createProxy(['utils', 'tokenUtils']);
+/** @type {typeof import('cat/scripts/utilities/uiUtils.mjs').default} */
 export const uiUtils = createProxy(['utils', 'uiUtils']);
+/** @type {typeof import('cat/scripts/utilities/workflowUtils.mjs').default} */
 export const workflowUtils = createProxy(['utils', 'workflowUtils']);
+/** @type {typeof import('cat/scripts/utilities/animationUtils.mjs').default} */
 export const animationUtils = createProxy(['utils', 'animationUtils']);
+/** @type {typeof import('cat/scripts/utilities/dialogUtils.mjs').default} */
 export const dialogUtils = createProxy(['utils', 'dialogUtils']);
+/** @type {typeof import('cat/scripts/utilities/summonUtils.mjs').default} */
 export const summonUtils = createProxy(['utils', 'summonUtils']);
+/** @type {typeof import('cat/scripts/utilities/compendiumUtils.mjs').default} */
+export const compendiumUtils = createProxy(['utils', 'compendiumUtils']);
+/** @type {typeof import('cat/scripts/utilities/folderUtils.mjs').default} */
+export const folderUtils = createProxy(['utils', 'folderUtils']);
