fa938107b4ea6f2c7545b379e3ea4e8031900944
Chris <chrisk123999@gmail.com>
2026-06-19 17:55:13 -0400

    More Blade Spell Work

--- scripts/macros/generic/bladeSpell.mjs
+++ scripts/macros/generic/bladeSpell.mjs
@@ -1,9 +1,9 @@
-import {actorUtils, automationUtils, dialogUtils, rollUtils, workflowUtils, genericUtils, activityUtils, effectUtils} from '../../proxy.mjs';
+import {actorUtils, automationUtils, dialogUtils, rollUtils, workflowUtils, genericUtils, activityUtils, effectUtils, tokenUtils} from '../../proxy.mjs';
 async function use({document, workflow}) {
     if (workflow.targets.size !== 1) return;
     const weapons = actorUtils.getEquippedWeapons(workflow.actor);
     if (!weapons.length) return;
-    const selectedWeapon = weapons.length === 1 ? weapons[0] : await dialogUtils.selectDocumentDialog(workflow.item.name, 'CHRISPREMADES.Macros.Generic.BladeSpell.Weapon', weapons);
+    const selectedWeapon = weapons.length === 1 ? weapons[0] : await dialogUtils.selectDocumentDialog(document.name, 'CHRISPREMADES.Macros.Generic.BladeSpell.Weapon', weapons);
     if (!selectedWeapon) return;
     const itemData = selectedWeapon.toObject();
     genericUtils.setProperty(itemData, 'flags.chris-premades.bladeSpell', true);
@@ -37,13 +37,62 @@ async function use({document, workflow}) {
     });
     const attackWorkflow = await workflowUtils.syntheticItemDataRoll(itemData, workflow.actor, Array.from(workflow.targets));
     const onHitEffect = automationUtils.getGenericConfigValue(document, 'chris-premades', 'bladeSpell', 'onHitEffect');
-    if (onHitEffect && attackWorkflow.hitTargets.size) {
-        const sourceEffect = workflow.item.effects.find(effect => effect.id === onHitEffect);
-        if (sourceEffect) {
-            const sourceEffectData = sourceEffect.toObject();
-            sourceEffectData.origin = sourceEffect.uuid;
-            sourceEffectData.duration = activityUtils.getEffectDuration(workflow.activity);
-            await effectUtils.createEffects(attackWorkflow.hitTargets.first().actor, [sourceEffectData]);
+    if (attackWorkflow.hitTargets.size) {
+        if (onHitEffect) {
+            const sourceEffect = workflow.item.effects.find(effect => effect.id === onHitEffect);
+            if (sourceEffect) {
+                const sourceEffectData = sourceEffect.toObject();
+                sourceEffectData.origin = sourceEffect.uuid;
+                sourceEffectData.duration = activityUtils.getEffectDuration(workflow.activity);
+                await effectUtils.createEffects(attackWorkflow.hitTargets.first().actor, [sourceEffectData]);
+            }
+        }
+        const activityOnHit = automationUtils.getGenericConfigValue(document, 'chris-premades', 'bladeSpell', 'activityOnHit');
+        if (activityOnHit) {
+            const activity = document.system.activities.get(activityOnHit);
+            if (activity) await workflowUtils.completeActivityUse(activity, Array.from(workflow.targets));
+        }
+        const effectNearby = automationUtils.getGenericConfigValue(document, 'chris-premades', 'bladeSpell', 'activityNearby');
+        const effectNearbyRange = automationUtils.getGenericConfigValue(document, 'chris-premades', 'bladeSpell', 'effectNearbyRange');
+        const effectNearbyTargets = automationUtils.getGenericConfigValue(document, 'chris-premades', 'bladeSpell', 'effectNearbyTargets');
+        if (effectNearby && effectNearbyRange && effectNearbyTargets) {
+            const sourceEffect = workflow.item.effects.find(effect => effect.id === effectNearby);
+            if (sourceEffect) {
+                const sourceEffectData = sourceEffect.toObject();
+                sourceEffectData.origin = sourceEffect.uuid;
+                sourceEffectData.duration = activityUtils.getEffectDuration(workflow.activity);
+                const targets = tokenUtils.findNearby(attackWorkflow.hitTargets.first().document, effectNearbyRange, {disposition: 'ally'});
+                let target;
+                if (targets.length <= effectNearbyTargets) {
+                    target = targets;
+                } else if (targets.length) {
+                    const context = effectNearbyTargets.length === 1 ? 'CHRISPREMADES.Macros.Generic.BladeSpell.SelectNearbyTarget' : 'CHRISPREMADES.Macros.Generic.BladeSpell.SelectNearbyTargets';
+                    const selection = await dialogUtils.selectTargetDialog(document.item.name, context, targets, {skipDeadAndUnconscious: false, type: effectNearbyTargets > 1 ? 'multiple' : 'one'});
+                    if (selection?.result) target = Array.isArray(selection.result) ? selection.result : [selection.result];
+                }
+                await effectUtils.createEffects(target.map(i => i.actor), [sourceEffectData]);
+            }
+        }
+        const activityNearby = automationUtils.getGenericConfigValue(document, 'chris-premades', 'bladeSpell', 'activityNearby');
+        if (activityNearby) {
+            const activity = document.system.activities.get(activityNearby);
+            if (activity) {
+                const activityNearbyTargets = activity.target.affects.count ?? 1;
+                const targets = tokenUtils.findNearby(attackWorkflow.hitTargets.first().document, activity.range.value, {disposition: 'ally'});
+                let target;
+                if (targets.length <= activityNearbyTargets) {
+                    target = targets;
+                } else if (targets.length) {
+                    const context = effectNearbyTargets.length === 1 ? 'CHRISPREMADES.Macros.Generic.BladeSpell.SelectNearbyTarget' : 'CHRISPREMADES.Macros.Generic.BladeSpell.SelectNearbyTargets';
+                    const selection = await dialogUtils.selectTargetDialog(document.item.name, context, targets, {
+                        skipDeadAndUnconscious: false, 
+                        type: activityNearbyTargets > 1 ? 'multiple' : 'one',
+                        maxAmount: activityNearbyTargets
+                    });
+                    if (selection?.result) target = Array.isArray(selection.result) ? selection.result : [selection.result];
+                }
+                if (target && target.length) await workflowUtils.completeActivityUse(activity, target);
+            }
         }
     }
 }
