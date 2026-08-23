86d9c666dfde29e0f9ca262e4a567d18ec1941eb
bacon-nugget <122384608+bacon-nugget@users.noreply.github.com>
2026-08-23 18:51:55 -0400

    clean up arcane recovery dialog prompt
    now uses dialogUtils.selectSpellSlots
    new plural util for selecting recovery or spending

--- scripts/macros/modern/classFeatures/wizard/arcaneRecovery.mjs
+++ scripts/macros/modern/classFeatures/wizard/arcaneRecovery.mjs
@@ -1,36 +1,24 @@
 import {actorUtils, automationUtils, dialogUtils, rollUtils} from '../../../../proxy.mjs';
 async function use({workflow}) {
+    if (!Object.values(workflow.actor.system.spells).some(s => s.max > 0 && s.value < s.max)) return;
     const formula = automationUtils.getConfigValue(workflow.item, 'formula');
     const bonuses = await automationUtils.calledEvent('arcaneRecoveryBonus', workflow.actor, {multiResult: true, canOverlap: true, data: {workflow}}) ?? [];
     const bonus = bonuses.reduce((total, value) => total + (value.bonus ?? 0), 0);
-    const spells = workflow.actor.system.spells;
     const totalSlots = (await rollUtils.rollDice(formula + ' + ' + bonus, {document: workflow.activity})).total;
-    const availableLevels = [];
-    const fields = [];
     const maxSlotLevel = automationUtils.getConfigValue(workflow.item, 'maxSlotLevel') ?? 5;
-    for (let i = 1; i <= maxSlotLevel; i++) {
-        const spellData = spells['spell' + i];
-        if (spellData?.max > 0 && spellData.value < spellData.max) {
-            availableLevels.push(i);
-            fields.push({
-                label: _loc('CHRISPREMADES.Macros.Modern.ArcaneRecovery.Slot', {slot: i}),
-                name: 'spell' + i,
-                options: {minAmount: 0, maxAmount: spellData.max - spellData.value, weight: i}
-            });
-        }
-    }
-    if (!fields.length) return;
-    const selection = await dialogUtils.selectAmounts(workflow.item.name, _loc('CHRISPREMADES.Macros.Modern.ArcaneRecovery.Context', {totalSlots}), fields, {totalMax: totalSlots});
-    if (!selection) return;
-    for (const i of availableLevels) {
-        const amount = selection['spell' + i] || 0;
-        if (amount > 0) await actorUtils.recoverSpellSlots(workflow.actor, i, {amount});
-    }
+    const selection = await dialogUtils.selectSpellSlots(workflow.actor, workflow.item.name, _loc('CHRISPREMADES.Macros.Modern.ArcaneRecovery.Context', {totalSlots}), {
+        maxLevel: maxSlotLevel,
+        maxAmount: totalSlots,
+        recover: true
+    });
+    if (!selection?.length) return;
+    await Promise.all(selection.map(s => actorUtils.recoverSpellSlots(workflow.actor, s.key, {amount: s.amount})));
 }
 export const arcaneRecovery = {
     name: 'Arcane Recovery',
     version: '2.0.0',
     rules: '2024',
+    notes: 'Use the "actorArcaneRecoveryBonus" called event (async) to modify the number of spell slots that can be recovered.\n\tData available: workflow.',
     roll: [
         {
             pass: 'itemRollFinished',
