dde9180a0157bd9251646de5cfdda19ac26b4fd5
Chris <chrisk123999@gmail.com>
2026-09-06 20:28:17 -0400

    Small Fix

--- scripts/macros/modern/monsters/construct/shieldGuardian/bound.mjs
+++ scripts/macros/modern/monsters/construct/shieldGuardian/bound.mjs
@@ -1,7 +1,7 @@
 import {activityUtils, actorUtils, automationUtils, tokenUtils, workflowUtils} from '../../../../../proxy.mjs';
 async function bind({document, workflow}) {
     if (!workflow.targets.size) return;
-    await automationUtils.setConfigValue(document, 'target', workflow.targets.first().actor.uuid);
+    await automationUtils.setConfigValue(document.item, 'target', workflow.targets.first().actor.uuid);
 }
 async function damaged({document, ditem, targetToken}) {
     if (!ditem.isHit || ditem.damageDetail[0]?.type === 'none' || (ditem.oldHP <= ditem.newHP && ditem.newTempHP >= ditem.oldTempHP)) return;
